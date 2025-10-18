73aca2f3023776c2274b116a8461ae6094bd7595
nominik <mike.nogai.spb@mail.ru>
2025-10-18 14:45:32 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        ArrayList<Integer> result = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         for (int i = 0; i < intList.size(); i++) {
             int y = intList.get(i);
             if (y > 0 && y % 2 == 0) {
